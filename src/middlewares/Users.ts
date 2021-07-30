80e760a3a1872e49b6e2fe64ad35ef0df2e51f3e
james dockal <jamesistranho@gmail.com>
2021-07-30 19:07:51 -0300

    feat: create UserMiddleware isLogged

--- src/middlewares/Users.ts
+++ src/middlewares/Users.ts
@@ -1,4 +1,5 @@
 import { Response, Request, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
 
 class UserMiddleware {
   providedLoginCredentials(req: Request, res: Response, next: NextFunction) {
@@ -10,6 +11,52 @@ class UserMiddleware {
     }
 
     next();
+  }
+
+  async isLogged(req: Request, res: Response, next: NextFunction) {
+    // get the header authorization where the token must be
+    const bearerHeader = req.headers["authorization"];
+
+    //if there is no authorization header, it will return a error
+    if (!bearerHeader) {
+      return res.status(401).json({
+        error:
+          "Not Authorized, you must have authorization header with 'Bearer ...token'!",
+      });
+    }
+
+    // separete the authauthorizationr header value into a array
+    // header value must be "Bearer ...token"
+    const splitedHeader = bearerHeader.split(" ");
+
+    // return an error if there is no Bearer word or token
+    if (splitedHeader.length < 2) {
+      return res
+        .status(400)
+        .json({ error: "Token bad formated, it should be 'Bearer ...token'" });
+    }
+
+    // get the word "Bearer" and the token
+    const [bearer, token] = splitedHeader;
+
+    // if the bearer is no written Bearer return a error
+    if (bearer !== "Bearer") {
+      return res
+        .status(400)
+        .json({ error: "Token bad formated, it should be 'Bearer ...token" });
+    }
+
+    // validate the token
+    try {
+      const secretKey = process.env.SECRET_KEY || "some_secret_key";
+      const userLevel = await verify(token, secretKey);
+      console.log("user level", userLevel);
+
+      // res.user_level = userLevel;
+      return next();
+    } catch (e) {
+      return res.status(400).json({ error: "Invalid Token" });
+    }
   }
 }
 
