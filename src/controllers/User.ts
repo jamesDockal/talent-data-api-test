1effa8b58f064397b02bdee0509b7093c7ab08f2
james dockal <jamesistranho@gmail.com>
2021-07-30 22:55:06 -0300

    fix: fix verify of the password on login

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -7,18 +7,17 @@ class UsersController {
     const { email, password } = req.body;
 
     const user = users.find((element) => element.email == email);
-    const userPassword = users.find((element) => element.password == password);
 
     if (!user) {
       return res.status(400).json({ error: "Email not found!" });
     }
 
-    if (!userPassword) {
+    if (user.password != password) {
       return res.status(400).json({ error: "Invalid Password!" });
     }
 
     const secretKey = process.env.SECRETKEY || "some_secret_key";
-    const token = await sign(user.userId, secretKey);
+    const token = await sign(user.roles[0], secretKey);
 
     res.json({ token: token });
   }
